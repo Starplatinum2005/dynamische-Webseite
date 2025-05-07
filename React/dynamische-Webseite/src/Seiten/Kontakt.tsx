b295c595438fe2d653f61c2d8534faa89d36fa97
Starplatinum2005 <stefanstanisic2005@gmail.com>
2025-05-07 14:26:55 +0200

    Kontaktformular

--- React/dynamische-Webseite/src/Seiten/Kontakt.tsx
+++ React/dynamische-Webseite/src/Seiten/Kontakt.tsx
@@ -1,4 +1,5 @@
 import './stylesheets/Kontakt.css'
+import { Kontaktformular } from '../Komponente/Kontaktformular'
 
 export function Kontakt (){
     return(
@@ -10,22 +11,7 @@ export function Kontakt (){
     <main>
     <section className="Kontakt">
         <h2>Kontaktformular</h2>
-        <form action="Sendebestätigung.html" className="Kontaktformular">
-            
-            <label htmlFor="Vorname">Vorname:</label>
-            <input type="text" id="Vorname" placeholder="Vorname" required/>
-        
-            <label htmlFor="Nachname">Nachname:</label>
-            <input type="text" id="Nachname" placeholder="Nachname" required/>
-        
-            <label htmlFor="E-mail">E-Mail:</label>
-            <input type="email" id="E-mail" placeholder="Ihre E-Mail-Adresse" required/>
-        
-            <label htmlFor="Ihr Anliegen">Ihr Anliegen:</label>
-            <textarea id="Ihr Anliegen" placeholder="Ihre Nachricht" rows={5} required></textarea>
-        
-            <button type="submit" className="button">Absenden</button>
-        </form>
+        <Kontaktformular/>
     </section>
     </main>
         </>

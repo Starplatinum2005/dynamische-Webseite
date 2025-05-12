e94a59290308955d9804a1118a7ebe7125959a41
Starplatinum2005 <stefanstanisic2005@gmail.com>
2025-05-12 13:33:48 +0200

    Bildungsprojekt

--- React/dynamische-Webseite/src/Seiten/Bildungsprojekt.tsx
+++ React/dynamische-Webseite/src/Seiten/Bildungsprojekt.tsx
@@ -1,19 +1,20 @@
 import './stylesheets/Bildungsprojekt.css';
+import { Bildungsprojekttext } from '../Objects/Bildungsprojekt';
 
 export function Bildungsprojekt (){
     return(
         <>
-            <main className='main_Bildungsprojekt'>
+        
+        <main className='main_Bildungsprojekt'>
 
-        <h1 className='h1_Bildungsprojekt'> Bildungsprojekte der Tauchschule Blue Ocean Dive</h1>
+        <h1 className='h1_Bildungsprojekt'> {Bildungsprojekttext.Bildungsprojekte.h2}</h1>
         <section className="Bildungsprojekte-container">
         <img className="Bildungsprojekte-image" src="https://cdn.pixabay.com/photo/2023/09/14/09/07/pollution-8252584_1280.jpg" alt = "Bildungsprojekte" />
         <article className="Bildungsprojekte-text">
             <p>  
-            Die Tauchschule Blue Ocean Dive sieht ihre Aufgabe nicht nur darin, Menschen das Tauchen beizubringen, sondern auch ein tiefes Verständnis für die Meereswelt und ihren Schutz zu vermitteln. 
-            Mit kostenlosen Bildungsprojekten richtet sich die Tauchschule besonders an Schüler und freiwillige Interessierte, um Wissen über den Schutz der Ozeane, ihrer Ökosysteme und Artenvielfalt zu verbreiten. 
-            <br/>
-            Denn nur wer die Meere versteht, kann sich nachhaltig für ihren Erhalt einsetzen.
+                {Bildungsprojekttext.Bildungsprojekte.p1}
+                <br/>
+                {Bildungsprojekttext.Bildungsprojekte.p2}
             </p>
         </article>
     </section>
@@ -23,13 +24,10 @@ export function Bildungsprojekt (){
     <section className="Projekte">
     <img className="Projekte-image" src="https://cdn.pixabay.com/photo/2024/02/25/15/36/wash-8595992_1280.jpg" alt = "Kin sammelt Müll" />
     <article className = "Projekte-text">
-        <h2 className='h2_Bildungsprojekte'> Meeres-Workshops für Schulen und Säuberungsaktionen</h2>
-        <p>Schüler spielen eine entscheidende Rolle, wenn es um die Zukunft unserer Meere geht. Daher bietet Blue Ocean Dive spezielle Programme für Schulen an, um Umweltbewusstsein und Wissen über die Unterwasserwelt zu fördern.
-        Interaktive Workshops erklären die Bedeutung der Meere für unser Klima und Leben. 
-        Die Schüler erfahren, wie Korallenriffe als Lebensraum für Tausende von Arten dienen und wie Plastikverschmutzung und Klimawandel diese bedrohen.</p>
+        <h2 className='h2_Bildungsprojekte'> {Bildungsprojekttext.Workshops.h2}</h2>
+        <p>{Bildungsprojekttext.Workshops.p1}</p>
         <br/>
-        <p>All diese Themen: Recycling, Schutz bedrohter Arten und verantwortungsbewusster Konsum werden praktisch, während gemeinsamen Säuberungsaktionen der Strände Mallorcas, den Schülern nähergebracht. 
-        </p>
+        <p>{Bildungsprojekttext.Workshops.p2}</p>
     </article> 
     </section>
 
@@ -39,33 +37,24 @@ export function Bildungsprojekt (){
     <section className="Vortragsreihen">
     <img className="Vortragsreihen-image" src="https://cdn.pixabay.com/photo/2022/11/02/13/58/meeting-7564970_1280.jpg" alt="Frau stellt ein Projekt vor"/>
     <article className = "Vortragsreihen-text">
-        <h2 className='h2_Bildungsprojekte'>Bürgerwissenschaftliche Projekte</h2> 
-        <p> 
-            Blue Ocean Dive lädt Taucher ein, an Forschungsprojekten teilzunehmen, die sich auf die Überwachung von Korallenriffen, Fischpopulationen oder die Auswirkungen von Verschmutzung konzentrieren. 
-            Die gesammelten Daten werden mit Wissenschaftlern geteilt, um ein besseres Verständnis der Meereswelt zu fördern.
-            Beispielweise helfen in einem Projekt Teilnehmer, Korallenbleiche zu dokumentieren oder Müll in Küstenregionen zu kartieren. Gerade Hobbytaucher und Wissenschaftsinteressierte sind eingeladen, daran teilzunehmen. 
-        </p>
-        <h2 className='h2_Bildungsprojekte'>Vortragsreihen und Info-Abende</h2>
-        <p>
-            In regelmäßigen Abständen organisiert die Tauchschule Vortragsabende, bei denen Experten über aktuelle Themen wie Klimawandel, Plastikverschmutzung oder nachhaltige Fischerei informieren.
-            Diese Veranstaltungen sind für die lokale Gemeinschaft und alle interessierten Besucher kostenlos zugänglich.
-            Das Ziel ist die Sensibilisierung für globale und lokale Herausforderungen der Ozeane. Die Besucher sind dazu eingeladen, über Themen zu diskutieren und den Austausch mit Fachleuten zu suchen. 
-       </p> 
+        <h2 className='h2_Bildungsprojekte'>{Bildungsprojekttext.Forschungsprojekte.h2}</h2> 
+        <p> {Bildungsprojekttext.Forschungsprojekte.p1} </p>
+        <h2 className='h2_Bildungsprojekte'>{Bildungsprojekttext.Forschungsprojekte.h2_2}</h2>
+        <p>{Bildungsprojekttext.Forschungsprojekte.h2_2}</p> 
     </article> 
     </section>
 
     <br/>
     <section>
-    <h2 className='h2_Bildungsprojekte'>  Engagement für die Gemeinschaft </h2>     
+    <h2 className='h2_Bildungsprojekte'>  {Bildungsprojekttext.Gemeinschaft.h2} </h2>     
     <p> 
-        Diese freiwilligen Bildungsprojekte sind für Blue Ocean Dive ein Herzensanliegen. 
-        Sie zeigen, dass Tauchen weit über den Sport hinausgeht - es ist ein Mittel, Menschen zu inspirieren, Wissen zu teilen und gemeinsam für den Erhalt der Ozeane zu kämpfen. Die Tauchschule lädt alle Interessierten ein, Teil dieser Projekte zu werden und mit Leidenschaft und Engagement die Unterwasserwelt zu erkunden und zu schützen.
+        {Bildungsprojekttext.Gemeinschaft.p1}
         <br/>
-        Gemeinsam können wir die Meere schützen - tauchen Sie mit uns ein! 
+        {Bildungsprojekttext.Gemeinschaft.p2}
         <br/>
     </p>
     </section>
     </main>
-     </>
+    </>
 );
 }

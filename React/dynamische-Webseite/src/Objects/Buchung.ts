a1317359c2032eb6b04fb9aee68bf4917b7ae00e
Starplatinum2005 <stefanstanisic2005@gmail.com>
2025-05-12 14:42:03 +0200

    Buchungsseite

---
+++ React/dynamische-Webseite/src/Objects/Buchung.ts
@@ -0,0 +1,49 @@
+export type Buchungstyp = {
+    h2: string,
+    p: string,
+    Preis: string,
+    Bild: string,
+    Alt: string
+}
+
+export const BuchungInfo = {
+    Warnung: {
+        h2: "Warnung",
+        p: "Bei dieser Website handelt es sich um ein studentisches Projekt"
+    },
+    Schnuppertauchen: {
+        h2: "Schnuppertauchen",
+        p: "Der perfekte Einstieg in die Unterwasserwelt für Neugierige und Anfänger. Probieren Sie das Tauchen unter Anleitung aus!",
+        Preis: "Preis: 60 €",
+        Bild: "https://cdn.pixabay.com/photo/2012/02/23/08/57/woman-15840_1280.jpg",
+        Alt: "Schnuppertauchen"
+    },
+    Delfintauchen: {
+        h2: "Delfintauchen",
+        p: "Erleben Sie ein unvergessliches Abenteuer beim Tauchen mit Delfinen. Perfekt für Naturliebhaber.",
+        Preis: "Preis: 110 €",
+        Bild: "https://cdn.pixabay.com/photo/2015/06/11/23/45/dolphin-806359_1280.jpg",
+        Alt: "Delfintauchen"
+    },
+    Korallentauchen: {
+        h2: "Korallentauchen",
+        p: "Entdecken Sie die faszinierende Welt der Korallenriffe. Geeignet für erfahrene Taucher.",
+        Preis: "Preis: 80 €",
+        Bild: "https://cdn.pixabay.com/photo/2016/04/26/22/35/coral-1355474_1280.jpg",
+        Alt: "Korallentauchen"
+    },
+    Tauchschein: {
+        h2: "Tauchschein",
+        p: "Erlernen Sie die Grundlagen des Tauchens mit einem professionellen Trainer. Ideal für Anfänger und Fortgeschrittene.",
+        Preis: "Preis: 300 €",
+        Bild: "https://cdn.pixabay.com/photo/2015/03/11/15/19/divers-668777_1280.jpg",
+        Alt: "Tauchschein"
+    },
+    Höhlentauchen: {
+        h2: "Höhlentauchen",
+        p: "Erkunden Sie die faszinierenden Höhlen unter Wasser und erleben Sie eine einzigartige Herausforderung.",
+        Preis: "Preis: 130 €",
+        Bild: "https://cdn.pixabay.com/photo/2012/02/23/08/35/below-15685_1280.jpg",
+        Alt: "Höhlentauchen"
+    }
+};

261a402a0f1690c7701a5a502c7d04755f82b3d5
Starplatinum2005 <stefanstanisic2005@gmail.com>
2025-05-19 20:43:49 +0200

    Projektboxen ausgelagert

---
+++ React/dynamische-Webseite/src/Komponente/ProjektBoxen.tsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+type ProjektBox = {
+    p: string,
+    img: string,
+    alt: string,
+    navigate: string
+}
+
+export function ProjektBoxen({Projekt} : {Projekt: ProjektBox}){
+    const navigate = useNavigate();
+    return(
+        <>
+            <article className="Thema">
+                  <img className="Themabild" src={Projekt.img} alt={Projekt.alt}/>
+                  <p>{Projekt.p}</p>
+                  <div className="Jetzt-buchen">
+                      <button
+                        aria-label="Link zum Naturschutz"
+                        onClick={() => navigate(Projekt.navigate)}>
+                        Mehr dazu
+                      </button>
+                    </div>
+              </article>
+        </>
+    )
+}
